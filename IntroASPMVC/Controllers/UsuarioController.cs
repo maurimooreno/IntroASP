9125f6bc23237f9af4de12c7de773cbc0ad18a18
memoreno <memoreno@kpmg.com.ar>
2023-06-29 15:32:28 -0300

    feat: Mejoras en el frontend y se agregó la funcionalidad para crear un usuario

--- IntroASPMVC/Controllers/UsuarioController.cs
+++ IntroASPMVC/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
 ﻿using IntroASPMVC.Models;
+using IntroASPMVC.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,38 @@ namespace IntroASPMVC.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Usuarios.ToListAsync());
+        }
+
+        public IActionResult CrearUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CrearUsuario(UsuarioViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool usuarioExiste = await _context.Usuarios.AnyAsync(u => u.Correo == model.Correo);
+                if (usuarioExiste)
+                {
+                    ModelState.AddModelError("Correo", "Ya existe un usuario con este correo electronico");
+                    return View(model);
+                }
+
+                var newUSer = new Usuario()
+                {
+                    Nombre = model.Nombre,
+                    Apellido = model.Apellido,
+                    Correo = model.Correo,
+                };
+                _context.Add(newUSer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
         }
     }
 }
---
+++ IntroASPMVC/Models/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntroASPMVC.Models.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Required]
+        public string Correo { get; set; }
+    }
+}
